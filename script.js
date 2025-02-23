4b887cd40a18e58ee9c65ac2b292e52789d879e5
Adrianogvs <dataengineer@adrianogvs.com.br>
2025-02-23 11:50:44 -0300

    feat: Ajustes de scroll-padding-top.

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const scrollTopBtn = document.getElementById('scrollTopBtn');
-  
-    // Exibe ou oculta o botão ao rolar a página
-    window.addEventListener('scroll', () => {
-      scrollTopBtn.style.display = window.pageYOffset > 300 ? 'flex' : 'none';
+  const scrollTopBtn = document.getElementById('scrollTopBtn');
+
+  // Exibe ou oculta o botão ao rolar a página
+  window.addEventListener('scroll', () => {
+    scrollTopBtn.style.display = window.pageYOffset > 300 ? 'flex' : 'none';
+  });
+
+  scrollTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
     });
-  
-    scrollTopBtn.addEventListener('click', () => {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-      });
+  });
+
+  // Efeito de expandir/recolher ao clicar na imagem do card
+  const cards = document.querySelectorAll('.screenshot-card');
+  cards.forEach(card => {
+    const img = card.querySelector('img');
+    card.addEventListener('click', () => {
+      img.classList.toggle('fullscreen');
     });
-  
-    // Efeito de expandir/recolher ao clicar na imagem do card
-    const cards = document.querySelectorAll('.screenshot-card');
-    cards.forEach(card => {
-      const img = card.querySelector('img');
-      card.addEventListener('click', () => {
-        img.classList.toggle('fullscreen');
-      });
-    });
+  });
 });
